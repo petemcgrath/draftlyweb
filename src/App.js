8e553816497b2b78192b0bccdf2ab61132cd14cc
Pete McGrath <pete.j.mcgrath@gmail.com>
2022-11-10 13:35:31 -0500

    Basic router

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { useState } from 'react';
 
 import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles'
+import { ThemeProvider } from '@mui/material/styles';
 import Container from '@mui/material/Container';
 import { styled } from '@mui/material/styles';
 
@@ -20,21 +20,29 @@ import theme from './customTheme';
 const Wrapper = styled(Container)(({ theme }) => ({
   backgroundColor: '#1E1E1E',
   minHeight: '100vh',
-  paddingBottom: '30px'
+  paddingBottom: '30px',
 }));
 
 export default function App({ ...props }) {
   const [step, setStep] = useState(1);
+
+  const basicRouter = () => {
+    switch (step) {
+      case 1:
+        return <Intro setStep={setStep} />;
+      case 2:
+        return <User />;
+      default:
+        return <div>whoops</div>;
+    }
+  };
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Wrapper>
         <Header setStep={setStep} />
-        {step === 1 ? 
-          <Intro setStep={setStep} /> :
-          <User />
-        }
+        {basicRouter()}
       </Wrapper>
     </ThemeProvider>
   );
